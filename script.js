79b00ab625a71c00e95d81e1846e9062f0c235e3
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-03 18:22:57 +0200

    .call() kontekst dla this

---
+++ script.js
@@ -0,0 +1,17 @@
+const sayHello = function(){
+    console.log('Hello ' + this.firstName + ' ' + this.lastName + '!')
+}
+
+const me = {
+    firstName: 'Bartek',
+    lastName: 'Nazwisko',
+    sayHello: sayHello
+}
+me.sayHello()
+
+const person = {
+    firstName: 'Ala',
+    lastName: 'Kotowicz',
+}
+
+sayHello.call(person) // funkcja .call() przyjmuje nowy kontekt dla this
