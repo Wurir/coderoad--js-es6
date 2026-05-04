048e60ddc709fad917351251d8c2647ea113a5da
WurirMacOs <wurir1994@gmail.com>
2026-05-04 23:43:31 +0200

    changing context and using thisArg inside ForEach fn

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const myOwnForEach = function(array, callback){
+const myOwnForEach = function (array, callback) {
 
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         const element = names[i]
         const index = i
         const array = names
-    }
 
-    callback(element, index, array)
+        callback(element, index, array)
+    }
 }
 
 const names = ['Ala', 'Ola', 'Ela']
 
-const myName = 'Bartek'
+window.myName = 'Bartek from window'
 
-names.forEach(
-    function(name, index, array){
-        console.log(this)
-    // console.log(`Hello ${name}, I\'m ${myName}.`)
-}, {} // second argument as context for 'this'
-)
+const greeter = function (name, index, array) {
+    // console.log(this)
+    console.log(`Hello ${name}, I\'m ${this.myName}.`)
+}
+
+names.forEach(greeter)
+
+myOwnForEach(names, greeter)
+
+names.forEach(greeter, {myName: 'Bartek from "thisArg"'})
