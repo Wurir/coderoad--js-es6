c3b16d4b1ce6852b430de8f82997b50253c4fb87
WurirMacOs <wurir1994@gmail.com>
2026-05-04 23:31:21 +0200

    do myOwnForEach fn

---
+++ script.js
@@ -0,0 +1,18 @@
+const myOwnForEach = function(array, callback){
+
+    for(let i = 0; i < array.length; i++){
+        const element = names[i]
+        const index = i
+        const array = names
+    }
+
+    callback(element, index, array)
+}
+
+const names = ['Ala', 'Ola', 'Ela']
+
+const myName = 'Bartek'
+
+names.forEach(function(name, index, array){
+    console.log(`Hello ${name}, I\'m ${myName}.`)
+})
