b2dbac3b512824f7794ba4ad94f65d3c6ea361e9
WurirMacOs <wurir1994@gmail.com>
2026-05-12 03:39:53 +0200

    Object constructor

--- Counter.js
+++ Counter.js
@@ -2,10 +2,10 @@
 
 const square1 = {
     color: 'red',
-    container: document.body,
-    render: function(){
+    container: document.querySelector('.square-1'),
+    render: function () {
         this.container.innerHTML = ''
-        
+
         const div = document.createElement('div')
         div.style.backgroundColor = this.color
         div.style.width = '100px'
@@ -16,3 +16,22 @@ const square1 = {
 }
 
 square1.render()
+
+// Object constructor
+
+const square2 = new Object()
+
+square2.color = 'green'
+square2.container = document.querySelector('.square-2')
+square2.render = function () {
+    this.container.innerHTML = ''
+
+    const div = document.createElement('div')
+    div.style.backgroundColor = this.color
+    div.style.width = '100px'
+    div.style.height = '100px'
+
+    this.container.appendChild(div)
+}
+
+square2.render()
