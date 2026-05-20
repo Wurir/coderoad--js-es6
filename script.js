73981ceec01c2278de9d8c2d39ef7d54c17bf696
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-20 17:54:34 +0200

    export default/import

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const task1 = {
-    text: 'Wynieś śmieci',
-    isCompleted: false,
-}
+const add = (a, b) => a + b
 
-const newTask1 = {
-    text: task1.text,
-    isCompleted : task1.isCompleted,
-    date: new Date()
-} // nieprzjemny sposob
-console.log(newTask1);
-
-
-const newTaskAssign = Object.assign(
-    {}, // podaje pusty obiekt do ktorego zostana przypisan wartosci
-    task1, // podaje obiekt, ktory wyladuje w obiekcie podanym na poczatku
-    {date: new Date()} // tutaj tez podaje obiekt z wartoscia, ktora zostanie dodana do nowo utworzoneg obiektu
-)
-console.log(newTaskAssign);
-
-console.log(task1 === newTask1); // false
-console.log(task1 === newTaskAssign); // false
-
-const newTaskSpread = { ...task1, date: new Date().getFullYear(), isCompleted: true}
-// najwygodniejszy sposob do tworzenia i modyfikacji obiektow, ES6
-console.log(newTaskSpread);
-
-console.log(task1 === newTaskSpread); // false
-
-
+export default add
