4c42830310ae4346d218d86dfea5a0af9496d4ec
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-21 17:38:13 +0200

    import calej zawartosci modulu naraz

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import addDefault, {add as addNamed, substract} from './math.js'
+import * as math from './math.js'
 
+console.log(typeof math)
+console.log(math)
+console.log(math.add === math.default)
+
+const add = math.default
+const addNamed = math.add
+const substract = math.substract
+
+console.log(add(3, 3));
 console.log(addNamed(2, 2));
 console.log(substract(2, 2));
-console.log(addDefault(3, 3));
 
