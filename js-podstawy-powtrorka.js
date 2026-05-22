0a26e510ceaee6382ae1b74cb20d3ccf1308eeed
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-22 16:24:08 +0200

    operatory arytmetyczne

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,5 +1,27 @@
-console.log(1 + "2");      // "12"  -> Liczba 1 jest zamieniana na string i łączona z "2"
-console.log("5" - 2);      // 3     -> Operator minus wymusza konwersję stringa "5" na liczbę
-console.log(true + 1);     // 2     -> W operacjach matematycznych true jest traktowane jako 1
-console.log(!!"" || 42);   // 42    -> Pusty string to "falsy", więc operator || zwraca drugą wartość
-console.log([] == ![]);    // true  -> Skomplikowana koercja: ![] daje false, a [] po konwersji na liczbę to 0
+var x = 2
+var y = 4
+
+var a = '2'
+var b = '4'
+
+console.log(x + y); // 6
+console.log(a + b); // '24'
+
+console.log(Number(x) + Number(y)); // 6
+console.log(Number(a) + Number(b)); // 6
+
+
+console.log(x - y); // -2
+console.log(a - b); // -2
+console.log(x - 'hello'); //NaN
+
+
+console.log(x * y); // 8
+console.log(a * b) // 8
+console.log(x * 'hello'); //NaN
+
+console.log(x / y); // 0.5
+console.log(a / b); // 0.5
+
+console.log(x % y); // 2
+console.log(a % b); // 0
