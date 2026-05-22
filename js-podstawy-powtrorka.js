646803847a4d8a6743cb0ffe0c773417ff2116db
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-22 16:09:28 +0200

    koercja

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,5 +1,5 @@
-'use strict'
-
-x = 5 
-//Reference error: x is not defined
-// działalo by bez 'use strict'
+console.log(1 + "2");      // "12"  -> Liczba 1 jest zamieniana na string i łączona z "2"
+console.log("5" - 2);      // 3     -> Operator minus wymusza konwersję stringa "5" na liczbę
+console.log(true + 1);     // 2     -> W operacjach matematycznych true jest traktowane jako 1
+console.log(!!"" || 42);   // 42    -> Pusty string to "falsy", więc operator || zwraca drugą wartość
+console.log([] == ![]);    // true  -> Skomplikowana koercja: ![] daje false, a [] po konwersji na liczbę to 0
