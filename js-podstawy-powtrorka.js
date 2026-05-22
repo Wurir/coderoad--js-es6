9a4e3453739e9a40abe70701ece9bb1186b8270d
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-22 17:04:02 +0200

    obiekty zagniezdzanie

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,33 +1,17 @@
 'use strict'
 
-var cat1 = {
-    name: 'Fluffy',
-    age: 3,
-    'strings can be property names': 'Bye',
-    123 : 123,
+var me = {
+    firstName: 'Bartek',
+    lastName: 'Bogdanowicz',
+    car: {
+        brand: 'Toyota',
+        model: 'Avensis',
+    },
 }
 
-// var 123 = 'Hello'
+me.name = me.firstName + ' ' + me.lastName
 
-console.log(cat1.name); //Fluffy
-console.log(cat1.age); //3
-console.log(cat1['strings can be property names']) //Bye
-
-var stringPropertyName = 'strings can be property names'
-console.log(cat1.stringPropertyName); // Bye
-
-console.log(cat1[123]); //123
-console.log(cat1['123']); //123
-
-console.log(cat1);
-
-
-console.log('---------------------------');
-
-
-var cat2 = {}
-
-cat2.name = 'Puszek'
-cat2.age = 12
-
-console.log(cat2);
+console.log(me);
+console.log(me.car.brand); //Toyota
+console.log(me.car.model) //Avensis
+console.log(me.name); // Bartosz Bogdanowicz
