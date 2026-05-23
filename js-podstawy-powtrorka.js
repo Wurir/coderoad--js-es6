39b7e227fffe533399d9485c4ca94fd8772b16ec
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-23 23:18:51 +0200

    IIFE calculator

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,9 +1,31 @@
 'use strict'
 
-var result = (function add(a, b){
-    return a + b
-})(1, 3)
+;(function () {
 
-// var result = add(1, 3)
+    function add(a, b) {
+        return a + b
+    }
 
-console.log(result)
+    var userInput1 = prompt('Please type a number!')
+    var number1 = Number(userInput1)
+
+    var userInput2 = prompt('Please type second number!')
+    var number2 = Number(userInput2)
+
+    var sum = add(number1, number2)
+
+    if (sum) {
+
+        alert('The result is: ' + sum)
+
+    } else {
+
+        if (sum === 0) {
+            alert('The result is: ' + sum)
+        } else {
+            alert('Sorry, you provided not a number!')
+        }
+
+    }
+
+})()
