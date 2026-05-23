fec3275cc77d71bdc141afeb21968902810a019f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-23 21:41:56 +0200

    funkcje i scope zmiennych

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,18 +1,26 @@
 'use strict'
 
-var me = {
-    firstName: 'Bartek',
-    lastName: 'Bogdanowicz',
+var func1 = console.log
+var result1 = console.log('Hello!')
+
+console.log(func1)
+console.log(result1)
+
+var x = 5
+var y = 10
+
+// add becomes a variable accessible in global scope
+function add(x, y) {
+    return x + y
 }
 
-var person1 = {
-    firstName: 'Alicja',
-    lastName: 'Kotowicz',
-}
+var func2 = add
+var result2 = add(1, 3)
 
-var people = [me, person1]
+console.log(func2)
+console.log(result2)
 
-person1.firstName = 'Ala'
-
-console.log(people);
- 
+// second argument is not defined
+// so y is undefined
+// and undefined + something === NaN
+console.log(add(5)) // NaN
