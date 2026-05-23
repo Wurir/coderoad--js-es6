bc24966759be65462176e0d68aab53d6d11b1a52
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-23 19:41:44 +0200

    obiekty referencja

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -16,7 +16,11 @@ var me = {
 
 me.name = me.firstName + ' ' + me.lastName
 
+firstName = 'Maciek' // strings are passed by value
+
+car.model = 'Corolla' // zmieniam wartosc w referencji
+
 console.log(me);
 console.log(me.car.brand); //Toyota
-console.log(me.car.model) //Avensis
+console.log(me.car.model) //Avensis => w linii 21 zmienione na Corolla
 console.log(me.name); // Bartosz Bogdanowicz
