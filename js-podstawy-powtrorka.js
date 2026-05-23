b38a0e8d8774c92ef577a9c39a03fe11daf9cfd8
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-23 23:04:11 +0200

    IIFE wstep

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,55 +1,9 @@
 'use strict'
 
-// function declaration / statement
-function add(a, b) {
+var result = (function add(a, b){
     return a + b
-}
+})(1, 3)
 
-// named function expressions
-var addExpression = function addExpression(a, b) {
-    return a + b
-}
+// var result = add(1, 3)
 
-// anonymous function expressions
-var addAnonymousExpression = function (a, b) {
-    return a + b
-}
-
-var func1 = function namedFunction(a, b) {
-    return a + b
-}
-var func2 = func1
-
-console.log(func1)
-console.log(func2)
-console.log(func1 === func2)
-
-var func3 = function (a, b) {
-    return a + b
-}
-var func4 = func3
-
-console.log(func3)
-console.log(func4)
-console.log(func3 === func4)
-
-// this throw ReferenceError
-// because name of named functions
-// is not available in global scope
-// so it is commented
-// console.log(namedFunction)
-
-// typeof event when variable is never defined
-// it returns undefined not ReferenceError
-console.log(typeof namedFunction)
-
-var namedFunctionVariable = function nameOfFunc(){
-    // named function name is available in local
-    // scope of that function, not in scopes above like global
-    console.log(nameOfFunc)
-}
-
-console.log(typeof nameOfFunc) // undefined
-
-// it logs function by name
-namedFunctionVariable()
+console.log(result)
