6872853bbccbcfae9e35777694eff548c6d77057
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-25 15:47:52 +0200

    this i kontekst wywolania funkcji

--- js-podstawy-powtrorka.js
+++ js-podstawy-powtrorka.js
@@ -1,21 +1,21 @@
-const makeCounter = function () {
-
-    let number = 0
-
-    const inc = function () {
-        number = number + 1
-    }
-
-    const status = function () {
-        console.log('Current number is: ' + number)
-    }
-
-    return {
-        inc: inc,
-        status: status,
-    }
-
+const inc = function () {
+    this.number = this.number + 1
 }
 
-const counter1 = makeCounter()
-const counter2 = makeCounter()
+const status = function () {
+    console.log('Current number is: ' + this.number)
+}
+
+const counter1 = {
+    name: 'counter1',
+    number: 0,
+    inc: inc,
+    status: status,
+}
+
+const counter2 = {
+    name: 'counter2',
+    number: 0,
+    inc: inc,
+    status: status,
+}
