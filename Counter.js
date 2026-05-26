1160c890ea66b497aa891b846aa8477969bba7dd
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-26 17:37:12 +0200

    Counter z uzyciem modułów

--- Counter.js
+++ Counter.js
@@ -1,47 +1,8 @@
-const Counter = function (selector) {
-    const container = document.querySelector(selector)
+console.log('Hello counter!')
 
-    if (!container) throw new Error('Container element not found')
+export let number = 0
 
-    this.selector = selector
-    this.container = container
-    this.number = 0
-}
+export const getNumber = () => number
 
-Counter.prototype.init = function () {
-    this.render()
-}
-
-Counter.prototype.render = function () {
-
-    this.container.innerText = ''
-
-    const h1 = document.createElement('h1')
-    const button = document.createElement('button')
-
-    h1.innerText = this.number
-    button.innerText = '+'
-
-    const self = this
-
-    button.addEventListener(
-        'click',
-        function () {
-            self.inc()
-        }
-    )
-
-    this.container.appendChild(h1)
-    this.container.appendChild(button)
-
-}
-
-Counter.prototype.inc = function () {
-    this.number = this.number + 1
-    this.render()
-}
-
-Counter.prototype.status = function () {
-    console.log('Current number is: ' + this.number)
-    return this.number
-}
+export const inc = () => number = number + 1
+export const dec = () => number = number - 1
