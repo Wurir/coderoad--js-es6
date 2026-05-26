7a9120831fbfc93e3178c5312bb739503375d477
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-26 17:54:09 +0200

    do Button for Counter

--- Counter.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello counter!')
-
-export let number = 0
-
-export const getNumber = () => number
-
-export const inc = () => number = number + 1
-export const dec = () => number = number - 1
