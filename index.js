f56eef1b888010bdbcabdde3120835e71d984836
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-27 17:34:39 +0200

    clear for webpack/libraries lessons

--- index.js
+++
@@ -1,7 +0,0 @@
-import { Counter } from './counter/Counter.js'
-import { DecreasingCounter } from './counter/DecreasingCounter.js'
-
-const counter1 = new Counter('.counter-1')
-counter1.init()
-const counter2 = new DecreasingCounter('.counter-2')
-counter2.init()
