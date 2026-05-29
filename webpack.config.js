af1e1f541efd4e3ee0ae8ccd376845b3d22a73b9
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-05-29 16:04:43 +0200

    configure HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    plugins: [new HtmlWebpackPlugin]
+    plugins: [new HtmlWebpackPlugin({
+        template: 'index.html'
+    })]
 }
