d790b9536d18e1fe09450e1cc5b7c341b5b93a7e
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-06-05 17:08:13 +0200

    template strings

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-const name = 'Bartosz'
-const lastName = 'Bogdanowicz'
-const carPropName = 'myCar'
+const string1 = 'Ala ma kota'
+const string2 = 'Ala\nma kota'
+// multiline strings
+const string3 = (
+    `Ala
+    ma kota`
+)
 
-const obj = {
-    // shorthand properties
-    name,
-    lastName,
-    // methods
-    sayHello() {
-        console.log('Hello ' + this.name + ' ' + this.lastName)
-    },
-    // computed properties
-    [carPropName]: {
-        brand: 'Toyota',
-    },
-}
+console.log(string1)
+console.log(string2)
+console.log(string3)
 
-console.log(obj)
+const name = 'Ola'
+const animal = 'psa'
 
-obj.sayHello()
+const string4 = name + ' ma ' + animal
+const string5 = `${name} ma ${animal}`
+
+console.log(string4)
+console.log(string5)
+
+const string6 = `The result of 2 + 2 is ${2 + 2}`
+
+console.log(string6)
