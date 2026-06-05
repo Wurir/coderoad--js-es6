759f9554d96b0710d0801492f5945ec9edb0cf63
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-06-05 23:40:03 +0200

    es11 operator ?? i nulish coalescing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-const me = {
-    name: 'Bartek',
-    car: {
-        brand: 'Toyota',
-    },
+let undefinedValue
+const nullValue = null
+const zeroValue = 0
+const emptyStringValue = ''
+const faleValue = false
+
+console.group('values')
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(zeroValue)
+console.log(emptyStringValue)
+console.log(faleValue)
+console.groupEnd('values')
+
+
+console.group('|| operator')
+console.log(undefinedValue || 'default value')
+console.log(nullValue || 'default value')
+console.log(zeroValue || 300)
+console.log(emptyStringValue || 'default value')
+console.log(faleValue || true)
+console.groupEnd('|| operator')
+
+console.group('?? operator')
+console.log(undefinedValue ?? 'default value')
+console.log(nullValue ?? 'default value')
+console.log(zeroValue ?? 300)
+console.log(emptyStringValue ?? 'default value')
+console.log(faleValue ?? true)
+console.groupEnd('?? operator')
+
+let inputValue = null
+const input = document.querySelector('.input')
+const result = document.querySelector('.result')
+
+const printResult = () => {
+    // if (inputValue === null) {
+    //     result.innerText = 'input was never touched'
+    //     return
+    // }
+
+    // result.innerText = inputValue || 'input is empty'
+
+    result.innerText = (inputValue ?? 'input was never touched') || 'input is empty'
 }
 
-console.log(me.name)
-console.log(me.lastName)
+printResult()
 
-console.log(me.car)
-console.log(me.car.brand)
-console.log(me.car.model)
-
-console.log(me.boat)
-console.log(me.boat && me.boat.brand)
-console.log(me.boat?.brand)
+input.addEventListener(
+    'input',
+    (event) => {
+        inputValue = event.target.value
+        printResult()
+    }
+)
